8b44206c642f3a029f7d78d38f703c00f6ece3aa
navjot turna <shamsherturna101@gmail.com>
2021-11-21 01:22:58 -0500

    typical text edit

--- client/src/PortfolioBox/Home/Profile.js
+++ client/src/PortfolioBox/Home/Profile.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import Typical from "react-typical";
 export default function Profile() {
     return (
         <div className='pr-container'>
             <div className="pr-parent">
                 <div className='pr-details'>
-                    <div className='col'></div>
+                    <div className='col'>
                    
-                    
+                    <div className="colicon">
                     <a href="#">
                         <i className="fa fa-google-plus-square" />
                     </a>
@@ -20,8 +20,40 @@ export default function Profile() {
                     <a href="https://www.linkedin.com/in/shamshersingh-/">
                     <i className="fa fa-linkedin"></i>
                     </a>
-                </div>
-            </div>
+                    </div>
+          </div>
+          <div className="pr-detailsname">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">Shamsher Singh</span>
+            </span>
+          </div>
+          <div className="pr-detailsrole">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Full stack Developer 💻",
+                    2500,
+                    "Machine Learning Engineer 📱",
+                    2500,
+                    "Java EE Developer ❤",
+                    2500,
+                  ]}
+                />
+              </h1>
+            </span>
+            <span className="pr-roletageline">
+             Web Applications and Artificial Intelligence Applications Developer
+            </span>
+          </div>
+
+        
         </div>
-    )
+       
+      </div>
+    </div>
+  );
 }
