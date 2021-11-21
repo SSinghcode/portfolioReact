e53acd19e883210e081c6bd9997e87974e6f894e
navjot turna <shamsherturna101@gmail.com>
2021-11-21 00:40:13 -0500

    profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-
+import Profile from './PortfolioBox/Home/Profile';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-     
+    <Profile/>
     </div>
   );
 }
---
+++ client/src/PortfolioBox/Home/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className='pr-container'>
+            <div className="pr-parent">
+                <div className='pr-details'>
+                    <div className='col'></div>
+                   
+                    
+                    <a href="#">
+                        <i className="fa fa-google-plus-square" />
+                    </a>
+                    <a href="https://www.instagram.com/turna.shamsher/">
+                        <i className="fa fa-instagram" />
+                    </a>
+                    <a href="https://github.com/SSinghcode?tab=repositories">
+                        <i className="fa fa-github" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/shamshersingh-/">
+                    <i className="fa fa-linkedin"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
