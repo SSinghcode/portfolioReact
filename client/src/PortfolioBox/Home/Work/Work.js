69c7e38fa209595b6ea15532128c9f3e2fa735f1
navjot turna <shamsherturna101@gmail.com>
2021-11-22 13:32:54 -0500

    minutes add

--- client/src/PortfolioBox/Home/Work/Work.js
+++ client/src/PortfolioBox/Home/Work/Work.js
@@ -11,50 +11,84 @@ export default function Work() {
     return (
         <div className="flex">
             <h3 id="work">Work</h3>
-            <div>
+            <div className="left">
                 <h3 className="heading">Code refactor</h3>
                 <img className="img1" src={web1} alt="web1" />
-                <a href="https://ssinghcode.github.io/code/" target="_blank">
-                GitHub Link
-                </a>   
-                
+                <div>
+                    <a href="https://github.com/SSinghcode/code" target="_blank">
+                        GitHub Link
+                    </a>
+                    <br />
+                    <a href="https://ssinghcode.github.io/code/" target="_blank">
+                        Deployed Link
+                    </a>
+                </div>
             </div>
 
             <div>
-            <h3 className="heading">Password Generator</h3>
-            <img className="img1" src={web2} alt="web2" />
-                <a href="https://ssinghcode.github.io/passwordgenerator/" target="_blank">
-                GitHub Link
-                </a>
-            </div>
-
-            <div>
-                <h3 className="heading">Quiz Challenge</h3>
-                <img className="img1" src={web3} alt="web3" />
-                <a href="https://ssinghcode.github.io/codequiz/" target="_blank">
-                GitHub Link
-                </a>
+                <div className="left">
+                    <h3 className="heading">Password Generator</h3>
+                    <img className="img1" src={web2} alt="web2" />
+                    <a href="https://github.com/SSinghcode/passwordgenerator" target="_blank">
+                        GitHub Link
+                    </a>
+                    <br />
+                    <a href="https://ssinghcode.github.io/passwordgenerator/" target="_blank">
+                        Deployed Link
+                    </a>
+                </div>
             </div>
             <div>
-                <h3 className="heading">Work Day Scheduler</h3>
-                <img className="img1" src={web4} alt="web4" />
-                <a href="https://ssinghcode.github.io/codequiz/" target="_blank">
-                GitHub Link
-                </a>
+                <div className="left">
+                    <h3 className="heading">Quiz Challenge</h3>
+                    <img className="img1" src={web3} alt="web3" />
+                    <a href="https://github.com/SSinghcode/codequiz" target="_blank">
+                        GitHub Link
+                    </a>
+                    <br />
+                    <a href="https://ssinghcode.github.io/codequiz/" target="_blank">
+                        Deployed Link
+                    </a>
+                </div>
             </div>
             <div>
-                <h3 className="heading">Note Taker</h3>
-                <img className="img1" src={web5} alt="web5" />
-                <a href="https://ssinghcode.github.io/codequiz/" target="_blank">
-                GitHub Link
-                </a>
+                <div className="left">
+                    <h3 className="heading">Work Day Scheduler</h3>
+                    <img className="img1" src={web4} alt="web4" />
+                    <a href="https://github.com/SSinghcode/dailyPlanner" target="_blank">
+                        GitHub Link
+                    </a>
+                    <br />
+                    <a href="https://ssinghcode.github.io/dailyPlanner/" target="_blank">
+                        Deployed Link
+                    </a>
+                </div>
             </div>
             <div>
-                <h3 className="heading">Fitness Traker</h3>
-                <img className="img1" src={web6} alt="web6" />
-                <a href="https://ssinghcode.github.io/codequiz/" target="_blank">
-                GitHub Link
-                </a>
+                <div className="left">
+                    <h3 className="heading">Note Taker</h3>
+                    <img className="img1" src={web5} alt="web5" />
+                    <a href="https://github.com/SSinghcode/noteTaker" target="_blank">
+                        GitHub Link
+                    </a>
+                    <br />
+                    <a href="https://shamshernotetaker.herokuapp.com/" target="_blank">
+                        Deployed Link
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div className="left">
+                    <h3 className="heading">Fitness Traker</h3>
+                    <img className="img1" src={web6} alt="web6" />
+                    <a href="https://github.com/SSinghcode/fitnessTracker" target="_blank">
+                        GitHub Link
+                    </a>
+                    <br />
+                    <a href="https://shamfitnesstarcker.herokuapp.com/" target="_blank">
+                        Deployed Link
+                    </a>
+                </div>
             </div>
 
         </div>
